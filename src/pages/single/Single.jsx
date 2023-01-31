21c4c350dadfc01c94327e83cfdc880cbf90d921
Christopher Morales <christopher.morales4752@gmail.com>
2023-01-31 18:19:02 -0400

    Added sidebar

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./single.scss"
+
+const Single = () => {
+  return (
+    <div className="single">
+        <h1 className='title'>single</h1>
+    </div>
+  )
+}
+
+export default Single;
